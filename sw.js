6606b947da1e4cdf4a63c03611b0a43121973eb5
memertemir-wq <memertemir@gmail.com>
2026-07-16 18:23:57 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,23 @@
-const CACHE = "ev-gozu-v1";
-const FILES = ["index.html", "viewer.html", "manifest.json"];
+const CACHE = "pampisup-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./viewer.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
 });
 
 self.addEventListener('fetch', e => {
