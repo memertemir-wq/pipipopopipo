4822ebe401f86f340ba1665d1c7cea7ebb7ef0e8
memertemir-wq <memertemir@gmail.com>
2026-07-16 17:49:52 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "ev-gozu-v1";
+const FILES = ["index.html", "viewer.html", "manifest.json"];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
